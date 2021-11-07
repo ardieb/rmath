73023f06ad3530b84d72f901a3afd026b582b00e
Arthur Burke <amb556@cornell.edu>
2021-11-07 17:08:33 -0500

    changed pricing model for implied volatility

--- src/options/black_scholes.rs
+++ src/options/black_scholes.rs
@@ -47,54 +47,134 @@ pub fn price_put_option<'graph, F: ag::Float>(
         - spot_price * stats::normal::cdf(g, &g.neg(d1), zero, one)
 }
 
-pub fn implied_call_volatility<'graph, F: ag::Float>(
-    g: &'graph ag::Graph<F>,
-    given_call_price: &ag::Tensor<'graph, F>,
-    spot_price: &ag::Tensor<'graph, F>,
-    time_to_maturity: &ag::Tensor<'graph, F>,
-    strike_price: &ag::Tensor<'graph, F>,
+pub fn implied_call_volatility<F: ag::Float>(
+    given_call_price: &ag::NdArray<F>,
+    given_spot_price: &ag::NdArray<F>,
+    given_time_to_maturity: &ag::NdArray<F>,
+    given_strike_price: &ag::NdArray<F>,
     risk_free_interest_rate: F,
-    option_count: usize,
     epochs: usize,
-) -> ag::Tensor<'graph, F> {
+) -> ag::NdArray<F> {
+    assert!(given_call_price
+        .shape()
+        .iter()
+        .zip(
+            given_spot_price.shape().iter().zip(
+                given_time_to_maturity
+                    .shape()
+                    .iter()
+                    .zip(given_strike_price.shape().iter())
+            )
+        )
+        .all(|(a, (b, (c, d)))| { *a == *b && *b == *c && *c == *d }));
     let rng = arr::ArrayRng::<F>::default();
-    let volatility_arr = arr::into_shared(rng.standard_uniform(&[option_count]));
+    let volatility_arr = arr::into_shared(rng.standard_uniform(given_call_price.shape()));
     let adam_state = ag::optimizers::adam::AdamState::new(&[&volatility_arr]);
 
     for _ in 0..epochs {
-        let volatility = g.variable(volatility_arr.clone());
-        let call_price_pred = price_call_option(g, spot_price, time_to_maturity, strike_price, &volatility, risk_free_interest_rate);
-        let mean_loss = g.sqrt(g.reduce_mean(g.square(call_price_pred - given_call_price), &[0], false));
-        let grads = &g.grad(&[&mean_loss], &[volatility]);
-        let update_ops: &[ag::Tensor<F>] = 
-            &ag::optimizers::adam::Adam::default().compute_updates(&[volatility], grads, &adam_state, g);
-        g.eval(update_ops, &[]);
+        ag::with(|g: &mut ag::Graph<F>| {
+            let volatility = g.variable(volatility_arr.clone());
+            let call_price = g.placeholder(&[-1]);
+            let spot_price = g.placeholder(&[-1]);
+            let time_to_maturity = g.placeholder(&[-1]);
+            let strike_price = g.placeholder(&[-1]);
+
+            let predicted_call_price = price_call_option(
+                g,
+                &spot_price,
+                &time_to_maturity,
+                &strike_price,
+                &volatility,
+                risk_free_interest_rate,
+            );
+            let mean_loss = g.reduce_mean(g.square(predicted_call_price - call_price), &[-1], false);
+            let grads = &g.grad(&[mean_loss], &[volatility]);
+            let update_ops = &ag::optimizers::adam::Adam::default().compute_updates(
+                &[volatility],
+                grads,
+                &adam_state,
+                g,
+            );
+
+            g.eval(
+                update_ops,
+                &[
+                    call_price.given(given_call_price.view().into_dyn()),
+                    spot_price.given(given_spot_price.view().into_dyn()),
+                    time_to_maturity.given(given_time_to_maturity.view().into_dyn()),
+                    strike_price.given(given_strike_price.view().into_dyn()),
+                ],
+            );
+        });
     }
-    g.variable(volatility_arr.clone())
+    let locked = volatility_arr
+        .read()
+        .expect("Could not read lock the volatility array"); 
+    locked.to_owned()
 }
 
-pub fn implied_put_volatility<'graph, F: ag::Float>(
-    g: &'graph ag::Graph<F>,
-    given_put_price: &ag::Tensor<'graph, F>,
-    spot_price: &ag::Tensor<'graph, F>,
-    time_to_maturity: &ag::Tensor<'graph, F>,
-    strike_price: &ag::Tensor<'graph, F>,
+pub fn implied_put_volatility<F: ag::Float>(
+    given_put_price: &ag::NdArray<F>,
+    given_spot_price: &ag::NdArray<F>,
+    given_time_to_maturity: &ag::NdArray<F>,
+    given_strike_price: &ag::NdArray<F>,
     risk_free_interest_rate: F,
-    option_count: usize,
     epochs: usize,
-) -> ag::Tensor<'graph, F> {
+) -> ag::NdArray<F> {
+    assert!(given_put_price
+        .shape()
+        .iter()
+        .zip(
+            given_spot_price.shape().iter().zip(
+                given_time_to_maturity
+                    .shape()
+                    .iter()
+                    .zip(given_strike_price.shape().iter())
+            )
+        )
+        .all(|(a, (b, (c, d)))| { *a == *b && *b == *c && *c == *d }));
     let rng = arr::ArrayRng::<F>::default();
-    let volatility_arr = arr::into_shared(rng.standard_uniform(&[option_count]));
+    let volatility_arr = arr::into_shared(rng.standard_uniform(given_put_price.shape()));
     let adam_state = ag::optimizers::adam::AdamState::new(&[&volatility_arr]);
 
     for _ in 0..epochs {
-        let volatility = g.variable(volatility_arr.clone());
-        let put_price_pred = price_put_option(g, spot_price, time_to_maturity, strike_price, &volatility, risk_free_interest_rate);
-        let mean_loss = g.sqrt(g.reduce_mean(g.square(put_price_pred - given_put_price), &[0], false));
-        let grads = &g.grad(&[&mean_loss], &[volatility]);
-        let update_ops: &[ag::Tensor<F>] = 
-            &ag::optimizers::adam::Adam::default().compute_updates(&[volatility], grads, &adam_state, g);
-        g.eval(update_ops, &[]);
+        ag::with(|g: &mut ag::Graph<F>| {
+            let volatility = g.variable(volatility_arr.clone());
+            let put_price = g.placeholder(&[-1]);
+            let spot_price = g.placeholder(&[-1]);
+            let time_to_maturity = g.placeholder(&[-1]);
+            let strike_price = g.placeholder(&[-1]);
+
+            let predicted_put_price = price_put_option(
+                g,
+                &spot_price,
+                &time_to_maturity,
+                &strike_price,
+                &volatility,
+                risk_free_interest_rate,
+            );
+            let mean_loss = g.reduce_mean(g.square(predicted_put_price - put_price), &[-1], false);
+            let grads = &g.grad(&[mean_loss], &[volatility]);
+            let update_ops = &ag::optimizers::adam::Adam::default().compute_updates(
+                &[volatility],
+                grads,
+                &adam_state,
+                g,
+            );
+
+            g.eval(
+                update_ops,
+                &[
+                    put_price.given(given_put_price.view().into_dyn()),
+                    spot_price.given(given_spot_price.view().into_dyn()),
+                    time_to_maturity.given(given_time_to_maturity.view().into_dyn()),
+                    strike_price.given(given_strike_price.view().into_dyn()),
+                ],
+            );
+        });
     }
-    g.variable(volatility_arr.clone())
+    let locked = volatility_arr
+        .read()
+        .expect("Could not read lock the volatility array"); 
+    locked.to_owned()
 }
