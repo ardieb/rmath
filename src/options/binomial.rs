da77b8dc1cd4025d0fa1e17ef36e286a59091ca8
Arthur Burke <amb556@cornell.edu>
2021-11-12 15:19:48 -0500

    refactored binomial pricing model for version 2.0.0

--- src/options/binomial.rs
+++ src/options/binomial.rs
@@ -1,11 +1,9 @@
 use autograd as ag;
+use autograd::array_gen as gen;
 use autograd::ndarray as nd;
-use autograd::ndarray_ext as arr;
+use autograd::tensor_ops as math;
 
 use num;
-use statrs;
-
-use crate::stats;
 
 /// Calculate the price of a call option based on the
 /// Binomial model for options pricing.
@@ -14,7 +12,6 @@ use crate::stats;
 /// a multidimensional set of inputs. All multi dimensional inputs
 /// must have the same shape.
 ///
-/// :param: `g` The graph which generated the tensors.
 /// :param: `s` The underlying stocks' prices per share.
 /// :param: `k` The options' strike prices per share.
 /// :param: `vol` The volatility of the stocks in decimal.
@@ -23,18 +20,30 @@ use crate::stats;
 /// :param: `t` The time until option maturity as decimal of a year.
 ///
 /// :return: `prices` The price of the options.
-pub fn call<'graph, F: ag::Float>(
-    g: &'graph ag::Graph<F>,
-    s: &ag::Tensor<'graph, F>,
-    k: &ag::Tensor<'graph, F>,
-    vol: &ag::Tensor<'graph, F>,
-    q: &ag::Tensor<'graph, F>,
-    r: F,
-    t: F,
-) -> ag::Tensor<'graph, F> {
-    ag::Tensor::builder()
-        .set_inputs(&[ag::tensor::Input::new(s), ag::tensor::Input::new(k), ag::tensor::Input::new(vol), ag::tensor::Input::new(q)])
-        .build(g, Bopm { r, t, ty: OptionType::Call })
+pub fn call<'graph, A, F: ag::Float>(s: A, k: A, vol: A, q: A, r: F, t: F) -> ag::Tensor<'graph, F>
+where
+    A: AsRef<ag::Tensor<'graph, F>>,
+{
+    let dim: i32 = -1;
+    let s = s.as_ref().expand_dims(&[dim]);
+    let k = k.as_ref().expand_dims(&[dim]);
+    let vol = vol.as_ref().expand_dims(&[dim]);
+    let q = q.as_ref().expand_dims(&[dim]);
+    let rs = (s/s) * r;
+    let ts = (s/s) * t;
+
+    let packed = math::concat(&[s, k, vol, q, rs, ts], 0);
+    packed.map(|packed| {
+        packed.map_axis(nd::Axis(0), |col| {
+            let s = col[0];
+            let k = col[1];
+            let vol = col[2];
+            let q = col[3];
+            let r = col[4];
+            let t = col[5];
+            eval_one_call(s, k, vol, q, r, t)
+        })
+    })
 }
 
 /// Calculate the price of a put option based on the
@@ -44,7 +53,6 @@ pub fn call<'graph, F: ag::Float>(
 /// a multidimensional set of inputs. All multi dimensional inputs
 /// must have the same shape.
 ///
-/// :param: `g` The graph which generated the tensors.
 /// :param: `s` The underlying stocks' prices per share.
 /// :param: `k` The options' strike prices per share.
 /// :param: `vol` The volatility of the stocks in decimal.
@@ -53,20 +61,31 @@ pub fn call<'graph, F: ag::Float>(
 /// :param: `t` The time until option maturity as decimal of a year.
 ///
 /// :return: `prices` The price of the options.
-pub fn put<'graph, F: ag::Float>(
-    g: &'graph ag::Graph<F>,
-    s: &ag::Tensor<'graph, F>,
-    k: &ag::Tensor<'graph, F>,
-    vol: &ag::Tensor<'graph, F>,
-    q: &ag::Tensor<'graph, F>,
-    r: F,
-    t: F,
-) -> ag::Tensor<'graph, F> {
-    ag::Tensor::builder()
-        .set_inputs(&[ag::tensor::Input::new(s), ag::tensor::Input::new(k), ag::tensor::Input::new(vol), ag::tensor::Input::new(q)])
-        .build(g, Bopm { r, t, ty: OptionType::Put })
-}
+pub fn put<'graph, A, F: ag::Float>(s: A, k: A, vol: A, q: A, r: F, t: F) -> ag::Tensor<'graph, F>
+where
+    A: AsRef<ag::Tensor<'graph, F>>,
+{
+    let dim: i32 = -1;
+    let s = s.as_ref().expand_dims(&[dim]);
+    let k = k.as_ref().expand_dims(&[dim]);
+    let vol = vol.as_ref().expand_dims(&[dim]);
+    let q = q.as_ref().expand_dims(&[dim]);
+    let rs = (s/s) * r;
+    let ts = (s/s) * t;
 
+    let packed = math::concat(&[s, k, vol, q, rs, ts], 0);
+    packed.map(|packed| {
+        packed.map_axis(nd::Axis(0), |col| {
+            let s = col[0];
+            let k = col[1];
+            let vol = col[2];
+            let q = col[3];
+            let r = col[4];
+            let t = col[5];
+            eval_one_put(s, k, vol, q, r, t)
+        })
+    })
+}
 
 #[derive(Copy, Clone, Eq, PartialEq)]
 enum OptionType {
@@ -74,101 +93,57 @@ enum OptionType {
     Put,
 }
 
-struct SingleBopm<F: ag::Float> {
-    s: F,
-    k: F,
-    vol: F,
-    q: F,
-    r: F,
-    t: F,
-    ty: OptionType
+fn eval_one_call<F: ag::Float>(s: F, k: F, vol: F, q: F, r: F, t: F) -> F {
+    eval_one(s, k, vol, q, r, t, OptionType::Call)
 }
 
-impl<F: ag::Float> SingleBopm<F> {
+fn eval_one_put<F: ag::Float>(s: F, k: F, vol: F, q: F, r: F, t: F) -> F {
+    eval_one(s, k, vol, q, r, t, OptionType::Put)
+}
 
-    fn eval(&self) -> F {
-        let dt: F = F::one() / F::from(365f64).unwrap();
-        let u: F = (self.vol * dt.sqrt()).exp();
-        let d: F = (-self.vol * dt.sqrt()).exp();
-        let p: F = (((self.r - self.q) * dt).exp() - d) / (u - d);
-        let n: usize = num::NumCast::from(self.t / dt).unwrap();
-        
-        let mut dp: ag::NdArray<F> = arr::zeros(&[n+1, n+1]);
+fn eval_one<F: ag::Float>(s: F, k: F, vol: F, q: F, r: F, t: F, ty: OptionType) -> F {
+    let dt: F = F::one() / F::from(365f64).unwrap();
+    let u: F = (vol * dt.sqrt()).exp();
+    let d: F = (-vol * dt.sqrt()).exp();
+    let p: F = (((r - q) * dt).exp() - d) / (u - d);
+    let n: usize = num::NumCast::from(t / dt).unwrap();
 
-        for j in 0..n+1 {
+    let mut dp: ag::NdArray<F> = gen::zeros(&[n + 1, n + 1]);
+
+    for j in 0..n + 1 {
+        let us = j as i32;
+        let steps = n as i32;
+        let stock_price = s * u.powi(2 * (us - steps));
+        let exercise_profit = match ty {
+            OptionType::Call => (stock_price - k).max(F::zero()),
+            OptionType::Put => (k - stock_price).max(F::zero()),
+        };
+        dp[[n, j]] = exercise_profit;
+    }
+
+    for i in (0..n).rev() {
+        for j in (0..n).rev() {
             let us = j as i32;
             let steps = n as i32;
-            let stock_price = self.s * u.powi(2 * (us - steps));
-            let exercise_profit = match self.ty {
-                OptionType::Call => (stock_price - self.k).max(F::zero()),
-                OptionType::Put => (self.k - stock_price).max(F::zero())
+            let stock_price = s * u.powi(2 * us - steps);
+            let exercise_profit = match ty {
+                OptionType::Call => (stock_price - k).max(F::zero()),
+                OptionType::Put => (k - stock_price).max(F::zero()),
             };
-            dp[[n, j]] = exercise_profit;
+            let decay = (-r * dt).exp();
+            let expected = p * dp[[i + 1, j + 1]] + (F::one() - p) * dp[[i + 1, j]];
+            let binom = decay * expected;
+            dp[[i, j]] = binom.max(exercise_profit);
         }
-
-        for i in (0..n).rev() {
-            for j in (0..n).rev() {
-                let us = j as i32;
-                let steps = n as i32;
-                let stock_price = self.s * u.powi(2 * us - steps);
-                let exercise_profit = match self.ty {
-                    OptionType::Call => (stock_price - self.k).max(F::zero()),
-                    OptionType::Put => (self.k - stock_price).max(F::zero())
-                };
-                let decay = (-self.r * dt).exp();
-                let expected = p * dp[[i+1, j+1]]
-                    + (F::one() - p) * dp[[i+1, j]];
-                let binom = decay * expected;
-                dp[[i, j]] = binom.max(exercise_profit);
-            }
-        }
-
-        dp[[0, 0]]
     }
 
-}
-
-struct Bopm<F: ag::Float> {
-    r: F,
-    t: F,
-    ty: OptionType,
-}
-
-impl<F: ag::Float> ag::op::Op<F> for Bopm<F> {
-    fn compute(&self, ctx: &mut ag::op::ComputeContext<F>) {
-        let s = ctx.input(0);
-        let k = ctx.input(1);
-        let vol = ctx.input(2);
-        let q = ctx.input(3);
-
-        let mut res: ag::NdArray<F> = arr::zeros(s.shape());
-        nd::Zip::from(&mut res).and(&s).and(&k).and(&vol).and(&q).apply(|res, &s, &k, &vol, &q| {
-            let opt = SingleBopm { s, k, vol, q, r: self.r, t: self.t, ty: self.ty };
-            *res = opt.eval();
-        });
-
-        ctx.append_output(res);
-    }
-
-    fn grad(&self, ctx: &mut ag::op::GradientContext<F>) {
-        todo!()
-    }
+    dp[[0, 0]]
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    use ag::tensor::Variable;
-
-    #[test]
-    fn test_expand() {
-        ag::with(|g| {
-            let arr: ag::Tensor<f64> = g.zeros(&[10]);
-            let arr2 = g.expand_dims(arr, &[-1i32]);
-            println!("{:?}", arr2.eval(&[]).expect("Failed").shape());
-        });
-    }
+    use autograd::prelude::*;
 
     #[test]
     fn test_call() {
@@ -176,18 +151,26 @@ mod tests {
         let strike_price = nd::array![30.].into_dyn();
         let volatility = nd::array![0.5654].into_dyn();
         let dividends = nd::array![0.].into_dyn();
+
+        let mut env = ag::VariableEnvironment::new();
+        env.name("s").set(spot_price);
+        env.name("k").set(strike_price);
+        env.name("vol").set(volatility);
+        env.name("q").set(dividends);
+
         let risk_free_interest_rate = 0.025;
         let time_to_maturity = 190. / 365.;
-        ag::with(|g| {
-            let s = g.variable(spot_price.clone());
-            let k = g.variable(strike_price.clone());
-            let vol = g.variable(volatility.clone());
-            let q = g.variable(dividends.clone());
+        env.run(|ctx| {
+            let s = ctx.variable("s");
+            let k = ctx.variable("k");
+            let vol = ctx.variable("vol");
+            let q = ctx.variable("q");
             let r = risk_free_interest_rate;
             let t = time_to_maturity;
-            let call_price = call(g, &s, &k, &vol, &q, r, t);
+
+            let call_price = call(&s, &k, &vol, &q, r, t);
             let c = call_price
-                .eval(&[])
+                .eval(ctx)
                 .expect("Could not evaluate call option price!");
             println!("{:?}", c.view());
         })
